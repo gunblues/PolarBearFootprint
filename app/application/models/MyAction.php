fd01c724b6306ad698d28f4762cff7ed65c45741
Administrator <admin@local.host>
2017-02-28 15:27:43 +0800

    modify my action

--- app/application/models/MyAction.php
+++ app/application/models/MyAction.php
@@ -4,8 +4,24 @@ class MyActionModel {
 
     //write your own action here
     static function execute($data) {
+
+        if (empty($data["url"])) {
+            return;
+        }
+
         MyRedis::init();
-		MyRedis::lpush("footprint", json_encode($data));
+        MyRedis::lpush("footprint", json_encode($data));
+
+        if (array_key_exists("away", $data) === false || $data["away"] !== 1) {
+            $host = parse_url($data["url"], PHP_URL_HOST);
+            MyRedis::lpush("page", json_encode(
+                array(
+                    "id" => $host . "_" . sha1($data["url"]),
+                    "url" => $data["url"],
+                    "udpated" => date("c")
+                )
+            ));
+        }
     }
 }
 
